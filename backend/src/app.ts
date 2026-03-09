efcca59ee962fbef60500174765ca7f4d82c2ef2
rohit <priyashakya2003@gmail.com>
2026-03-09 23:18:57 +0530

    set up prisma with user , post example tables in database rohit

---
+++ backend/src/app.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+// middleware
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+  res.send("Backend running");
+});
+
+
+export default app;
