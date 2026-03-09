f6389afbd2a6a991dfef3e63009b85196e57ac81
AryanDevX <aryan27.official@gmail.com>
2026-03-09 14:32:48 +0530

    Setup ESLint and Prettier for Google Style and also gitignore

---
+++ eslint.config.mjs
@@ -0,0 +1,37 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.strict,
+  pluginReact.configs.flat.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parserOptions: {
+        project: [
+          './tsconfig.json',
+          './frontend/tsconfig.json',
+          './backend/tsconfig.json',
+        ],
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'error',
+      'no-console': 'warn',
+    },
+  },
+  eslintConfigPrettier,
+);
