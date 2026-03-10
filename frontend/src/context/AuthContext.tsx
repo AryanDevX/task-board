23da8ce0e81124aca32796109319cc35abadf470
AryanDevX <aryan27.official@gmail.com>
2026-03-10 16:13:05 +0530

    Created AuthContext

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,23 +1,24 @@
 import React, { createContext, useReducer, useContext, type ReactNode } from 'react';
 
-export type GlobalRole = 'Global Admin' | 'User';
+export type UserRole = 'Global Admin' | 'Project User';
 
 export interface User {
   id: string;
   name: string;
   email: string;
   avatar: string | null;
-  role: GlobalRole;
+  role: UserRole;
 }
 
 export interface AuthState {
   user: User | null;
   isAuthenticated: boolean;
-  isLoading: boolean;
+  isLoading: boolean; 
+  token: string | null; //For JWT token.
 }
 
 export type AuthAction =
-  | { type: 'LOGIN'; payload: User }
+  | { type: 'LOGIN'; payload: {user: User, token: string} }
   | { type: 'LOGOUT' }
   | { type: 'UPDATE_AVATAR'; payload: string }
   | { type: 'SET_LOADING'; payload: boolean };
@@ -27,6 +28,7 @@ const initialState: AuthState = {
   user: null,
   isAuthenticated: false,
   isLoading: true,
+  token: null,
 };
 
 //Reducer:
@@ -36,15 +38,16 @@ const authReducer = (state: AuthState, action: AuthAction): AuthState => {
       return {
         ...state,
         isAuthenticated: true,
-        user: action.payload,
+        user: action.payload.user,
+        token: action.payload.token,
         isLoading: false,
       };
     case 'LOGOUT':
-      return { ...state, isAuthenticated: false, user: null, isLoading: false };
+      return { ...state, isAuthenticated: false, user: null, token: null, isLoading: false };
     case 'UPDATE_AVATAR':
+      if(!state.user) return state;
       return {
-        ...state,
-        user: state.user ? { ...state.user, avatar: action.payload } : null,
+        ...state, user: { ...state.user, avatar: action.payload },
       };
     case 'SET_LOADING':
       return { ...state, isLoading: action.payload };
@@ -54,16 +57,17 @@ const authReducer = (state: AuthState, action: AuthAction): AuthState => {
 };
 
 //Context
+//Adding the dispatch function with AuthState in AuthContextType
 export interface AuthContextType extends AuthState {
   dispatch: React.Dispatch<AuthAction>;
 }
+//Creating context
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
-//Provider:
-export const AuthProvide: React.FC<{ children: ReactNode }> = ({
-  children,
-}) => {
+//Provider: That provide data to all UI components nested inside the wrapper.
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [state, dispatch] = useReducer(authReducer, initialState);
+
   return (
     <AuthContext.Provider value={{ ...state, dispatch }}>
       {children}
@@ -71,7 +75,7 @@ export const AuthProvide: React.FC<{ children: ReactNode }> = ({
   );
 };
 
-//Hook
+//Hook(Receiver): Prevent other components from using AuthContext that are not wrapped inside AuthProvider.
 export const useAuth = (): AuthContextType => {
   const context = useContext(AuthContext);
   if (context === undefined) {
