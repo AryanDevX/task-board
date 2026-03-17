5e45f14428cf5d29da1621fb195782f6abaa1d5c
rohit <priyashakya2003@gmail.com>
2026-03-17 17:32:17 +0530

    comment and notification api files added

---
+++ frontend/src/api/comment.api.ts
@@ -0,0 +1,39 @@
+import { apiFetch } from "./client";
+import { type Comment } from "../types/models";
+
+export const commentApi = {
+
+  // Get all comments for a task
+  getCommentsByTask: (taskId: string , projectId:string ): Promise<Comment[]> =>
+    apiFetch(`projects/${projectId}/tasks/${taskId}/comments`),
+
+  // Create a comment
+  createComment: (
+    taskId: string, projectId:string ,
+    data: {
+      content: string;
+    }
+  ): Promise<Comment> =>
+    apiFetch(`projects/${projectId}/tasks/${taskId}/comments`, {
+      method: "POST",
+      body: JSON.stringify(data),
+    }),
+
+  // Update a comment
+  updateComment: (
+    commentId: string, projectId:string , taskId:string, 
+    data: {
+      content: string;
+    }
+  ): Promise<Comment> =>
+    apiFetch(`projects/${projectId}/tasks/${taskId}/comments/${commentId}`, {
+      method: "PUT",
+      body: JSON.stringify(data),
+    }),
+
+  // Delete a comment
+  deleteComment: (commentId: string , taskId:string , projectId: string): Promise<void> =>
+    apiFetch(`projects/${projectId}/tasks/${taskId}/comments/${commentId}`, {
+      method: "DELETE",
+    }),
+};
---
+++ frontend/src/api/notification.api.ts
@@ -0,0 +1,16 @@
+import { apiFetch } from "./client";
+import { type Notification } from "../types/models";
+
+export const notificationApi = {
+
+  // Get all notifications for current user
+  getNotifications: (projectId:string): Promise<Notification[]> =>
+    apiFetch(`/projects/${projectId}/notifications`),
+
+  // Mark a single notification as read
+  markAsRead: (notificationId: string, projectId:string ): Promise<Notification> =>
+    apiFetch(`/projects/${projectId}/notifications/${notificationId}/read`, {
+      method: "PUT",
+    }),
+
+};
